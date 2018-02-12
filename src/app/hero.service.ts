f018780fd37be4108ef49431fb4f20ce31981460
Subhajit Mandal <sumand@microsoft.com>
2018-02-11 18:25:22 -0800

    tutorial till Routing: implementation

---
+++ src/app/hero.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable()
+export class HeroService {
+  private heroesUrl = 'api/heroes';  // URL to web api
+  private heroUrl = 'detail/';
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add('HeroService: ' + message);
+  }
+
+  getHeroes(): Observable<Hero[]> {
+    // Todo: send the message _after_ fetching the heroes
+    // return of(HEROES);
+    /** GET heroes from the server */
+    return this.http.get<Hero[]>(this.heroesUrl);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    // Todo: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
+}
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,20 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
 
-  constructor() { }
+export class HeroesComponent implements OnInit {
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
 }
+
+// export class HeroesComponent implements OnInit {
+  
+//   heroes: Hero[];
+//   selectedHero: Hero;
+
+//   constructor(private heroService: HeroService) { }
+
+//   ngOnInit() {
+//     this.getHeroes();
+//   }
+
+//   onSelect(hero){
+//     this.selectedHero = hero;
+//   }
+//   getHeroes(): void {
+//     this.heroService.getHeroes()
+//         .subscribe(heroes => this.heroes = heroes);
+//   }
+// }
